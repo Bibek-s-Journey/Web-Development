3333d228c0aeef9bc8ac3c3c93a0be5b2fe4979a
Bibek Tamang <bibek7here@gmail.com>
2023-11-21 22:17:09 +0545

    responsive website

--- OllySnack/Source/script.js
+++ OllySnack/Source/script.js
@@ -18,3 +18,26 @@ logo.forEach((element,index) => {
         manual(index);
     });
 });
+
+
+// footer section
+let footer_top = document.querySelector(".footer-top");
+let contact_bottom = document.querySelector(".contact-bottom");
+let social_media = document.querySelector(".social-media");
+let bottom_right = document.querySelector(".bottom-right");
+let service_top = document.querySelector(".service-top");
+window.addEventListener("resize", (e) => {
+    if (window.innerWidth <= 992) {
+        console.log(window.innerWidth)
+        footer_top.append(contact_bottom);
+        footer_top.append(social_media);
+        service_top.append(bottom_right)
+    } else {
+        let our_contact = document.querySelector(".our-contact");
+        our_contact.appendChild(contact_bottom);
+        let contact_top = document.querySelector(".contact-top");
+        contact_top.appendChild(social_media);
+        let footer_bottom = document.querySelector(".footer-bottom");
+        footer_bottom.appendChild(bottom_right);
+    }
+})
